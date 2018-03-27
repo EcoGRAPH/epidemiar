33278fb8e1a13b43fc8c32af07c4f6b01c700c6f
Dawn Nekorchuk <Dawn.Nekorchuk@sdstate.edu>
2018-03-27 16:57:19 -0500

    corrected match.arg call, else if statement

--- R/add_datefields.R
+++ R/add_datefields.R
@@ -1,12 +1,12 @@
 add_datefields <- function(df, type = "ISO"){
-  match.arg(system, c("ISO", "CDC"))
+  type <- match.arg(type)
   # get morb df (or accepts any df with Date field) and adds year and week numbers
   if (type == "ISO"){
     df <- df %>%
       #add iso wk/yr
       mutate(week_epidemiar = epidemiar::epiweek(Date, system = "who"),
              year_epidemiar = epidemiar::epiyear(Date, system = "who"))
-  } else if (system == "CDC"){
+  } else if (type == "CDC"){
     #add CDC epi wk/yr
     mutate(week_epidemiar = epidemiar::epiweek(Date, system = "cdc"),
            year_epidemiar = epidemiar::epiyear(Date, system = "cdc"))

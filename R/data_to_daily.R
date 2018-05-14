a170295bc74d83da710614f4402e1596acba36fe
Dawn Nekorchuk <Dawn.Nekorchuk@sdstate.edu>
2018-05-14 17:24:53 -0500

    Corrected field name back to original value field.  This may mask interpolated env data identified in run_epidemia, but unless known which env var are not daily orginally, you don't know how to mark values.

--- R/data_to_daily.R
+++ R/data_to_daily.R
@@ -17,14 +17,12 @@ data_to_daily <- function(data_notdaily, valuefield, interpolate = TRUE){
     complete(Date = full_seq(c(min(data_notdaily$Date), max(data_notdaily$Date)), 1)) %>%
     ungroup()
 
-  #Note: unable to get group_by_at to use -var_names properly inside function (testing line by line runs okay)
-
   if (interpolate){
     data_1day <- data_1day %>%
       #should handle all grouping/categories, and therefore any ragged data
       group_by_at(vars(-Date, -!!quo_valuefield)) %>%
       #will not extrapolate beyond last known value, that will happen inside run_epidemia()
-      mutate(val_epidemiar = epidemiar::na_approx(!!quo_valuefield)) %>%
+      mutate(!!quo_name(quo_valuefield) := epidemiar::na_approx(!!quo_valuefield)) %>%
       #finish by ungrouping
       ungroup()
   }

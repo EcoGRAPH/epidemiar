e9496cfd609a45f7830d3676c63173782bdfec71
michdn <28901045+michdn@users.noreply.github.com>
2019-02-05 15:26:03 -0600

    Added different named variable from match.arg; spaces to make code more readable

--- R/add_datefields.R
+++ R/add_datefields.R
@@ -5,17 +5,22 @@
 #'
 
 add_datefields <- function(df, type = "ISO"){
-  type <- match.arg(type, c("ISO", "CDC"))
-  # get morb df (or accepts any df with obs_date field) and adds year and week numbers
-  if (type == "ISO"){
+
+  week_type <- match.arg(type, c("ISO", "CDC"))
+
+  # adds year and week numbers
+  if (week_type == "ISO"){
     df <- df %>%
       #add iso wk/yr
       dplyr::mutate(week_epidemiar = lubridate::isoweek(obs_date),
                     year_epidemiar = lubridate::isoyear(obs_date))
-  } else if (type == "CDC"){
+
+  } else if (week_type == "CDC"){
     #add CDC epi wk/yr
     dplyr::mutate(week_epidemiar = lubridate::epiweek(obs_date),
                   year_epidemiar = lubridate::epiyear(obs_date))
   }
+
   df
+
 }

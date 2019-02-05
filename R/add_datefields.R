3ab36ce55f909609bbcf1d698cdb9489b8238d0a
michdn <28901045+michdn@users.noreply.github.com>
2019-02-04 18:34:11 -0600

    Removed conflictingly-named functions. Old epiweek/epiyear custom functions have been replaced with (newer) lubridate functions of same name.

--- R/add_datefields.R
+++ R/add_datefields.R
@@ -5,17 +5,17 @@
 #'
 
 add_datefields <- function(df, type = "ISO"){
-  type <- match.arg(type)
+  type <- match.arg(type, c("ISO", "CDC"))
   # get morb df (or accepts any df with obs_date field) and adds year and week numbers
   if (type == "ISO"){
     df <- df %>%
       #add iso wk/yr
-      dplyr::mutate(week_epidemiar = epidemiar::epiweek(obs_date, system = "who"),
-                    year_epidemiar = epidemiar::epiyear(obs_date, system = "who"))
+      dplyr::mutate(week_epidemiar = lubridate::isoweek(obs_date),
+                    year_epidemiar = lubridate::isoyear(obs_date))
   } else if (type == "CDC"){
     #add CDC epi wk/yr
-    dplyr::mutate(week_epidemiar = epidemiar::epiweek(obs_date, system = "cdc"),
-                  year_epidemiar = epidemiar::epiyear(obs_date, system = "cdc"))
+    dplyr::mutate(week_epidemiar = lubridate::epiweek(obs_date),
+                  year_epidemiar = lubridate::epiyear(obs_date))
   }
   df
 }

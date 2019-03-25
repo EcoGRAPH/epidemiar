f14581c46e708edda1d2f2e3fa33c3f87acc42b4
michdn <28901045+michdn@users.noreply.github.com>
2019-03-25 15:17:23 -0500

    Edited @examples to remove error triggered by having .R

--- R/run_epidemia.R
+++ R/run_epidemia.R
@@ -96,7 +96,7 @@
 #' For more details see the vignette on the output data:
 #' \code{vignette("output-report-data", package = "epidemiar")}
 #'
-#'@examples See model_forecast_script.R in epidemiar-demo for full example: https://github.com/EcoGRAPH/epidemiar-demo
+#'@examples "See model_forecast_script in epidemiar-demo for full example: https://github.com/EcoGRAPH/epidemiar-demo"
 #'
 #'@export
 #'

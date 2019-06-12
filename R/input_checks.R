7f42748749a914b579b5fd863ae489ab753ec37f
michdn <28901045+michdn@users.noreply.github.com>
2019-06-12 16:35:50 -0500

    Corrected nesting of model cached input checks

--- R/input_checks.R
+++ R/input_checks.R
@@ -248,7 +248,7 @@ input_check <- function(epi_data,
   }
 
 
-# Models & Caching --------------------------------------------------------
+  # Models & Caching --------------------------------------------------------
 
   #use model_cached not old model_obj
   if (!is.null(model_obj) & is.null(model_cached)){
@@ -263,23 +263,21 @@ input_check <- function(epi_data,
     if (all(c("model_obj", "model_info") %in% names(model_cached))){
 
       #if model looks okay so far, then check further
-      if(!model_flag){
-        #make sure model_choice matches between cached model and settings.
-        #model choice already checked
-        if (!model_cached$model_info$model_choice == model_choice){
-          err_flag <- TRUE
-          err_msgs <- paste(err_msgs, "The model choice of the given cached model, \"", model_cached$model_info$model_choice, "\", does not match the current setting of 'model_choice' of \"", model_choice, "\".\n")
-        } #end model_choice
+      #make sure model_choice matches between cached model and settings.
+      #model choice already checked
+      if (!model_cached$model_info$model_choice == model_choice){
+        err_flag <- TRUE
+        err_msgs <- paste(err_msgs, "The model choice of the given cached model, \"", model_cached$model_info$model_choice, "\", does not match the current setting of 'model_choice' of \"", model_choice, "\".\n")
+      } #end model_choice
 
-        #make sure given model (if given) is a regression object (using basic "lm" as test)
-        #model_cached$model_obj
-        classes <- class(model_cached$model_obj)
-        if (!"lm" %in% classes){
-          err_flag <- TRUE
-          err_msgs <- paste(err_msgs, "The object in 'model_cached$model_obj' is not a regression object, found classes are: ", classes, ".\n")
-        } #end lm check
+      #make sure given model (if given) is a regression object (using basic "lm" as test)
+      #model_cached$model_obj
+      classes <- class(model_cached$model_obj)
+      if (!"lm" %in% classes){
+        err_flag <- TRUE
+        err_msgs <- paste(err_msgs, "The object in 'model_cached$model_obj' is not a regression object, found classes are: ", classes, ".\n")
+      } #end lm check
 
-      } #end if !model_flag
 
       #end if names
     } else {
@@ -294,7 +292,6 @@ input_check <- function(epi_data,
   # model_cached$model_info$date_created
   # model_cached$model_info$known_epi_range$max
   #but will probably give decent error messages on their own if missing.
-  # hhahahah, nope.
 
 
   # Control lists -----------------------------------------------------------
